5d0d25d2ac3d730148db63ddfb7e54910dcd53c0
azhar shaikh <44742531+rahzaazhar@users.noreply.github.com>
2020-05-27 12:53:58 +0530

    run multiple configs

---
+++ run_expts.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+import numpy as np
+import torch
+from train import run_learn_to_grow
+import L2G_config as M
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_name',help='name of Config to be used')
+    arg = parser.parse_args()
+    opts = getattr(M, arg.config_name)
+    for name,config in opts.items():
+      print('Loading.....',name+'_Config')
+      run_learn_to_grow(config)
