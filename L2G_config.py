7f5d22955f118b4c8f9b2a70a9652611099b54f9
azhar shaikh <44742531+rahzaazhar@users.noreply.github.com>
2020-05-28 12:14:07 +0530

    Update L2G_config.py

--- L2G_config.py
+++ L2G_config.py
@@ -19,25 +19,25 @@ class L2BG_Config():
 
 
 #config1 To find out whwther a randomly grown network performs worse than RSA strategy 
-config1 = L2BG_Config(exp_name='Random_growth',exp_dir='L2G_graphs/',datamode='smnist',sim_strat='random_growth',
+config1 = L2BG_Config(exp_name='Random_growth',exp_dir='./',datamode='smnist',sim_strat='random_growth',
 						config_name='config2')
 #config2 To train on CIFAR100 using RSA_sim
-config2 = L2BG_Config(exp_name='RSA_sim',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config2 = L2BG_Config(exp_name='RSA_sim',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config2')
 #config3 train task one for more iterations and subsequent tasks for lesser iterations select + most dissimilar task (10-5)
-config3 = L2BG_Config(exp_name='RSA_sim_1',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config3 = L2BG_Config(exp_name='RSA_sim_1',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config3')
 #config4 train task one for more iterations and subsequent tasks for lesser iterations + select most similar task (10-5)
-config4 = L2BG_Config(exp_name='RSA_sim_2',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config4 = L2BG_Config(exp_name='RSA_sim_2',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config4')
 #config5 To train on datasets by freezing weights of previous tasks
-config5 = L2BG_Config(exp_name='RSA_sim_2_freeze',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config5 = L2BG_Config(exp_name='RSA_sim_2_freeze',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config5',freeze_past=True)
 #config 6 To train 10 CIFAR Tasks each for 10 epochs wo freeze
-config6 = L2BG_Config(exp_name='CIFAR100_10',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config6 = L2BG_Config(exp_name='CIFAR100_10',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config6',n_tasks=10)
 #config 7 To train 10 CIFAR Tasks each for 10 epochs w freeze
-config7 = L2BG_Config(exp_name='CIFAR100_10_freeze',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config7 = L2BG_Config(exp_name='CIFAR100_10_freeze',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
 						config_name='config7',freeze_past=True,n_tasks=10)
 #config_CIFAR100_alpha config_pmnist_alpha config_pmnist_alpha 
 #above configs are to study the effect of alpha on model selection and accuracy
@@ -58,17 +58,17 @@ for al in alphas:
 
 
 ########################tests and sanity checks#################################### 
-config_pmnist_test = L2BG_Config(exp_name='pmnist_test',exp_dir='L2G_graphs/',datamode='pmnist',sim_strat='RSA',
+config_pmnist_test = L2BG_Config(exp_name='pmnist_test',exp_dir='./',datamode='pmnist',sim_strat='RSA',
                         config_name='config_pmnist',freeze_past=False,n_tasks=2,epochs=1)
 
-config_CIFAR100_test = L2BG_Config(exp_name='CIFAR100_test',exp_dir='L2G_graphs/',datamode='CIFAR100',sim_strat='RSA',
+config_CIFAR100_test = L2BG_Config(exp_name='CIFAR100_test',exp_dir='./',datamode='CIFAR100',sim_strat='RSA',
                         config_name='config_pmnist',freeze_past=False,n_tasks=2,epochs=1)
 
-config_smnist_test = L2BG_Config(exp_name='smnist_test',exp_dir='L2G_graphs/',datamode='smnist',sim_strat='RSA',
+config_smnist_test = L2BG_Config(exp_name='smnist_test',exp_dir='./',datamode='smnist',sim_strat='RSA',
                         config_name='config_pmnist',freeze_past=False,n_tasks=2,epochs=1)
 
 #config 8 To train VDD_test 
-config8 = L2BG_Config(exp_name='VDD_test',exp_dir='L2G_graphs/',datamode='VDD',sim_strat='RSA',
+config8 = L2BG_Config(exp_name='VDD_test',exp_dir='./',datamode='VDD',sim_strat='RSA',
                         config_name='config8',freeze_past=False,n_tasks=2,epochs=1)
 
 #config_san_1 To check if freezing is working or not
